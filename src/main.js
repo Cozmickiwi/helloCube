93f3aef516ff35aa8776fe639682a5695ee76be5
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-21 21:03:42 +1300

    create scene and camera

--- src/main.js
+++ src/main.js
@@ -7,3 +7,18 @@ import {
   Scene,
   WebGLRenderer,
 } from 'three';
+
+// Scene container
+const container = document.getElementById('scene-container');
+// Create scene
+const scene = new Scene();
+// Scene background color
+scene.background = new Color('skyblue');
+// Create camera
+const fov = 35;
+const aspect = container.clientWidth / container.clientHeight;
+const near = 0.1;
+const far = 100;
+
+const camera = new PerspectiveCamera(fov, aspect, near, far);
+camera.position.set (0, 0, 10);
