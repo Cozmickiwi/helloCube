52ce0d7fd43a1abdea5bc7ea6d050c95a9302028
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-21 20:48:28 +1300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import {
+  BoxBufferGeometry,
+  Color,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from 'https://cdn.skypack.dev/three@0.136.2';
+
+// just waiting for your beautiful creations!
